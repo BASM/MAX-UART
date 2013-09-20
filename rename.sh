b670dbff009b8e3407c2d69b80b00a69efe63d06
Leonid Myravjev <asm@asm.pp.ru>
2013-09-21 01:18:19 +0400

    Fix rename.sh script for new version of KiCAD

--- rename.sh
+++ rename.sh
@@ -2,7 +2,9 @@
 
 echo "Rename screipt"
 
+
 PRJ=$1
+PRJ="max232"
 
 if [ "$PRJ" = "" ] ; then
   echo "BAD"
@@ -16,11 +18,15 @@ mkdir $PRJ
 cp $PRJ-Front.gtl $PRJ/$PRJ.GTL
 cp $PRJ-Back.gbl  $PRJ/$PRJ.GBL
 
-cp $PRJ-SoldP_Front.gtp $PRJ/$PRJ.GTS
-cp $PRJ-SoldP_Back.gbp  $PRJ/$PRJ.GBS
+#cp $PRJ-SoldP_Front.gtp $PRJ/$PRJ.GTS
+cp $PRJ-F_Paste.gtp $PRJ/$PRJ.GTS
+#cp $PRJ-SoldP_Back.gbp  $PRJ/$PRJ.GBS
+cp $PRJ-B_Paste.gbp $PRJ/$PRJ.GBS
 
-cp $PRJ-SilkS_Front.gto $PRJ/$PRJ.GTO
-cp $PRJ-SilkS_Back.gbo  $PRJ/$PRJ.GBO
+#cp $PRJ-SilkS_Front.gto $PRJ/$PRJ.GTO
+cp $PRJ-F_SilkS.gto $PRJ/$PRJ.GTO
+#cp $PRJ-SilkS_Back.gbo  $PRJ/$PRJ.GBO
+cp $PRJ-B_SilkS.gbo $PRJ/$PRJ.GBO
 
 cp $PRJ.drl $PRJ/$PRJ.TXT
 
